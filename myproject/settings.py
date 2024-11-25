4f79dd46da2bd8757d179ce4a4de2a57e8f09369
Gupta Manish Pappu <141218115+MANISH3600@users.noreply.github.com>
2024-11-26 04:01:47 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,9 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'guptamanish1006@gmail.com'
-EMAIL_HOST_PASSWORD = 'khuttsofgkkbqoqs'  # Use the app password here
-
 
 # Default URL for login and logout redirects
 LOGIN_URL = '/login/'
